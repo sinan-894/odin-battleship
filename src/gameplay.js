fd9b95db30f4edba19e30b8f7b0735723b90531b
sinan-894 <mohammedsinan1011@gmail.com>
2025-07-06 20:44:40 +0530

    Create Src directory with source code
    >src created
    >gameplay.js created
    >test file created
    >wrote Object ship
    >wrote Object GameBoard
    >wrote a method to  place ship
    > tested the method
    
    Return methods of Ship objects

---
+++ src/gameplay.js
@@ -0,0 +1,74 @@
+
+
+export function Ship(length){
+    
+    let numberOfHits = 0
+
+    const hit = ()=>{
+        numberOfHits++
+    }
+
+    const isSunk = ()=>{
+        return (0 >= length-numberOfHits)?true:false 
+    }
+
+    return {length,hit,isSunk}
+
+}
+
+export function GameBoard(){
+
+    const state = []
+
+    for(let i=0;i<10;i++){
+        state.push(new Array(10).fill(0))
+    }
+
+    const placeShip = (x,y,length,isHorizontal)=>{
+        if(
+            x>9 || x<0 || y>9 || y<0 ||
+            (isHorizontal && y+length>9)||
+            (!isHorizontal && x+length>9)
+        ) return null
+        
+        let counter = 0
+        if(isHorizontal){
+            while(!state[x][y] && counter<length){
+                state[x][y] = 1
+                y++
+                counter++
+            }
+            while(counter>0 && counter<length){
+                y--
+                state[x][y] = 0
+                counter--
+            }
+        }
+        else{
+            
+            while(!state[x][y] && counter<length){
+                state[x][y] = 1
+                x++
+                counter++
+            }
+            while(counter>0 && counter<length){
+                x--
+                state[x][y] = 0
+                counter--
+            }
+
+        }
+    }
+
+    const getState =()=>state
+
+
+
+    return {placeShip,getState}
+
+
+    
+    
+
+
+}
---
+++ src/gamplay.test.js
@@ -0,0 +1,87 @@
+import { GameBoard } from "./gameplay"
+
+
+describe('Gameboard',()=>{
+    let board
+
+    beforeEach(()=>{
+        board = GameBoard()
+    })
+
+    describe('Place Ship',()=>{
+
+        let boardArray
+        
+        beforeEach(()=>{
+            boardArray = []
+            for(let i=0;i<10;i++){
+                boardArray.push(new Array(10).fill(0))
+            }
+        })
+
+        test('Place Ship at  12 horizontal',()=>{
+        
+            
+            boardArray[1][2] = 1
+            boardArray[1][3] = 1
+            boardArray[1][4] = 1
+            boardArray[1][5] = 1
+            boardArray[1][6] = 1
+    
+            board.placeShip(1,2,5,true)
+            expect(board.getState(board.getState())).toEqual(boardArray)
+        })
+    
+        test('Place Ship at  12 vertical',()=>{
+            
+            boardArray[1][2] = 1
+            boardArray[2][2] = 1
+            boardArray[3][2] = 1
+            boardArray[4][2] = 1
+            boardArray[5][2] = 1
+    
+            board.placeShip(1,2,5,false)
+            expect(board.getState()).toEqual(boardArray)
+        })
+    
+        test('Place Ship out of bounds',()=>{
+            board.placeShip(1,8,5,true)
+            expect(board.getState()).toEqual(boardArray)
+        })
+    
+        test('Place Ship out of bounds',()=>{
+            board.placeShip(10,8,5,false)
+            expect(board.getState()).toEqual(boardArray)
+        })
+
+        test('Place Ship In occupied area horizontal',()=>{
+            boardArray[1][2] = 1
+            boardArray[2][2] = 1
+            boardArray[3][2] = 1
+            boardArray[4][2] = 1
+            boardArray[5][2] = 1
+
+            board.placeShip(1,2,5,false)
+            board.placeShip(2,1,4,true)
+            expect(board.getState()).toEqual(boardArray)
+
+        })
+
+        test('Place Ship In occupied area vertical',()=>{
+            boardArray[1][2] = 1
+            boardArray[1][3] = 1
+            boardArray[1][4] = 1
+            boardArray[1][5] = 1
+            boardArray[1][6] = 1
+
+            board.placeShip(1,2,5,true)
+            board.placeShip(0,2,4,false)
+            console.log(board.getState())
+            expect(board.getState()).toEqual(boardArray)
+
+        })
+    })
+
+    
+
+})
