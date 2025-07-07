9c744e4fa5479bd557b0eece54fc504aa11f9cd6
sinan-894 <mohammedsinan1011@gmail.com>
2025-07-07 14:45:42 +0530

    Write a function to genrate game field
    >the game field function should return a
    container div that have two grids for user and
    opponent by user genrateGrid function

--- src/grid.js
+++ src/grid.js
@@ -1,5 +1,5 @@
 
- export function generateGridBoard(user){
+ export function generateGridBoard(user,onCellPress){
     const gridContainer = document.createElement('div');
     gridContainer.classList.add('grid-container')
     gridContainer.id = user
@@ -8,6 +8,7 @@
         for(let j=0;j<10;j++){
             let cell = document.createElement('div');
             cell.classList.add('cell-div');
+            cell.addEventListener('click',()=>onCellPress(cell))
             cell.id = `${rowID[i]}${j+1}`
             gridContainer.appendChild(cell)
         }
@@ -16,6 +17,23 @@
     return gridContainer
 }
 
-function setBackground(div,length){
-    div.classList.add(`len-${length}`)
+
+
+export function genarateGameField(user,opponent){
+    const userGrid = generateGridBoard(user.userName,(cell)=>{
+        console.log(cell.id)
+    })
+    addShipsToUserGrid(userGrid,user)
+
+    const opponentGrid = generateGridBoard(opponent.userName,(cell)=>{
+        console.log(cell.id)
+    })
+
+    const container = document.createElement('div');
+    container.classList.add('game-field')
+    container.appendChild(userGrid);
+    container.appendChild(opponentGrid)
+
+    return container
+
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import { genarateGameField } from "./grid.js";
+
+const user = {userName :'user'}
+const opp =  {userName : 'opp'}
+
+document.body.appendChild(genarateGameField(user,opp))
 
 
